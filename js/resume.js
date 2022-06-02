974b4dd9789535d1d5c29c99617ebff6bef4105b
Kushal Methukupally <105840305+Kushal-Methukupally@users.noreply.github.com>
2022-06-02 00:22:10 -0700

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -1,28 +1,162 @@
-(function($) {
-  "use strict"; // Start of use strict
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset='UTF-8'>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="shortcut icon" href="img/website.ico">
+  <meta name="description" content="YourDescriptionHere">
+  <meta name="author" content="862095186">
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+  <title>Kushal Methukupally - Business Supply Chain Management</title>
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#sideNav'
-  });
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
 
-})(jQuery); // End of use strict
+  <!-- Custom styles for this template -->
+  <link href="css/resume.min.css" rel="stylesheet">
+
+</head>
+<link rel="shortcut icon" href="img/website.ico">
+
+<body id="page-top">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <span class="d-block d-lg-none">Kushal Methukupally</span>
+      <span class="d-none d-lg-block">
+        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/kushal_m.jpg" alt="Profile Picture">"
+      </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  </nav>
+
+  <div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
+      <div class="w-100">
+        <h1 class="mb-3">Kushal Methukupally
+        </h1>
+        <div class="subheading mb-5">Business Supply Chain Management📚</div>
+        <p class="lead mb-4"><br> My name is Kushal Methukupally and I am currently a undergraduate student at The University of California, Riverside. I am curretnly pursuing a Bachelors in Business Adminstartion with a concentration in Supply Chain Management.<br>
+
+        My primary interests lie in the field of Business Analytics, Data Analytics and Supply Chain Management. I have relevant work expericne and background knowledge in the feild of business based on my previous experiences and course work.  <br><br>
+
+        My current plan is to finish my bachelors at the Univeristy of California, Riverside with an expected graduation date of Decemeber 2022. I plan to go out in the workforce to gain expericne within the field of business. 
+        </p>
+        <div class="social-icons">
+          <a target="_blank" id="socialicon-1" href="https://www.linkedin.com/in/kushal-methukupally-7350a4199/">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a target="_blank" id="socialicon-4" href="mailto:61264k@gmail.com">
+            <i class="fa fa-envelope"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <hr class="m-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+      <div class="w-100">
+        <h2 class="mb-5">Experience</h2>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5 experience">
+          <div class="resume-content">
+            <h3 class="mb-0">Retail Sales Associate	</h3>
+            <div class="subheading mb-3"><a target="_blank" href="https://www.calvinklein.us/en/mens-clothing/mens-apparel/mens-shop-all">Calvin Klein</a></div>
+            <p>
+              • Helped in explaining knowledge of product to promote brand and promotions <br> 
+              • Focused on customer engagement and experience <br>
+              • Maintained visual presentation standards while managing time and prioritizing tasks <br>
+              • Worked as a member of a team to accomplish goalsset forth in the store <br>
+            </p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">June 2021 - September 2021</span>
+          </div>
+        </div>
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5 experience">
+          <div class="resume-content">
+            <h3 class="mb-0">Comfort Theory Product Research Intern</h3>
+            <div class="subheading mb-3"><a target="_blank" href="">Self Business</a></div>
+            <p>
+              • Conducted product research using Jungle Scout to understand what products are optimal to sell on
+eCommerce platforms on customer keyword searches <br>
+              • Assisted and engaged with customer inquiries and supported team management<br>
+            </p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">May 2021 - August 2021</span>
+          </div>
+        </div>
+
+
+        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5 experience">
+          <div class="resume-content">
+            <h3 class="mb-0">Brand Ambassador & Customer Service Specialist</h3>
+            <div class="subheading mb-3"><a target="_blank" href="https://www.ralphlauren.com/">Ralph Lauren</a></div>
+            <p>
+            • Successfully demonstrated the ability to grow and maintain positive working relationships with
+customers, management and co-workers <br>
+            • Helped in thinking of new creative ways to engage clients and reach out to a wider client base<br>
+            • Engaged in networking and sourcing talent events and supported inventory management<br>
+            
+
+          </p>
+          </div>
+          <div class="resume-date text-md-right">
+            <span class="text-primary">June 2019 - August 2019</span>
+          </div>
+        </div>
+        <h2 class="mb-5">Education</h2>
+<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5 education">
+    <div class="resume-content">
+        <h3 class="mb-0">Arizona State Univeristy</h3>
+        <div class="subheading mb-3">Bachelors: Business Adminstration</div>
+        <div>Concentration: Logistics, Materials, and Supply Chain Management</div>
+    </div>
+    <div class="resume-date text-md-right">
+        <span class="text-primary">August 2018 - December 2019</span>
+    </div>
+</div>
+        <h2 class="mb-5">Education</h2>
+<div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5 education">
+    <div class="resume-content">
+        <h3 class="mb-0">University of California, Riverside</h3>
+        <div class="subheading mb-3">Bachelors: Business Administration</div>
+        <div>Concentartion: Logistics, Materials, and Supply Chain Management</div>
+    </div>
+    <div class="resume-date text-md-right">
+        <span class="text-primary">September 2019 - December 2022</span>
+    </div>
+</div>
+<h2 class="mb-5">Portfolio</h1>
+<center>
+    <img src="img/certificate.png" width="500px"><br>
+    <img src="img/my_calvins.jpg" width="500px"><br>
+    <img src="img/Ralph-Lauren-Logo.jpg" width="500px"><br>
+</center>
